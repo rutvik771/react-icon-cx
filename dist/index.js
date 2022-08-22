e99e4403369f670def3b08e11e28fd8976b67d14
Rutvik Patel <patelrutvik039@gmail.com>
2022-08-22 18:17:10 +0530

    first version

---
+++ dist/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var _extends = require('@babel/runtime/helpers/extends');
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+function _interopNamespace(e) {
+  if (e && e.__esModule) return e;
+  var n = Object.create(null);
+  if (e) {
+    Object.keys(e).forEach(function (k) {
+      if (k !== 'default') {
+        var d = Object.getOwnPropertyDescriptor(e, k);
+        Object.defineProperty(n, k, d.get ? d : {
+          enumerable: true,
+          get: function () { return e[k]; }
+        });
+      }
+    });
+  }
+  n["default"] = e;
+  return Object.freeze(n);
+}
+
+var _extends__default = /*#__PURE__*/_interopDefaultLegacy(_extends);
+var React__namespace = /*#__PURE__*/_interopNamespace(React);
+
+var _jsxFileName = "D:\\react-icon-library\\src\\AppGridMenuSvgrepoCom.js";
+
+const SvgAppGridMenuSvgrepoCom = props => /*#__PURE__*/React__namespace.createElement("svg", _extends__default["default"]({
+  xmlns: "http://www.w3.org/2000/svg",
+  xmlSpace: "preserve",
+  style: {
+    fillRule: "evenodd",
+    clipRule: "evenodd",
+    strokeLinejoin: "round",
+    strokeMiterlimit: 2
+  },
+  viewBox: "0 0 0 0"
+}, props, {
+  __self: undefined,
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 4,
+    columnNumber: 3
+  }
+}), /*#__PURE__*/React__namespace.createElement("path", {
+  d: "M6 22c-2.208 0-4 1.792-4 4s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4Zm0 2a2 2 0 1 1-.001 4.001A2 2 0 0 1 6 24Zm10-2c-2.208 0-4 1.792-4 4s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4Zm0 2a2 2 0 1 1-.001 4.001A2 2 0 0 1 16 24Zm10-2c-2.208 0-4 1.792-4 4s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4Zm0 2a2 2 0 1 1-.001 4.001A2 2 0 0 1 26 24ZM6 12c-2.208 0-4 1.792-4 4s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4Zm0 2a2 2 0 1 1-.001 4.001A2 2 0 0 1 6 14Zm10-2c-2.208 0-4 1.792-4 4s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4Zm0 2a2 2 0 1 1-.001 4.001A2 2 0 0 1 16 14Zm10-2c-2.208 0-4 1.792-4 4s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4Zm0 2a2 2 0 1 1-.001 4.001A2 2 0 0 1 26 14ZM6 2C3.792 2 2 3.792 2 6s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4Zm0 2a2 2 0 1 1-.001 4.001A2 2 0 0 1 6 4Zm10-2c-2.208 0-4 1.792-4 4s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4Zm0 2a2 2 0 1 1-.001 4.001A2 2 0 0 1 16 4Zm10-2c-2.208 0-4 1.792-4 4s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4Zm0 2a2 2 0 1 1-.001 4.001A2 2 0 0 1 26 4Z",
+  __self: undefined,
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 16,
+    columnNumber: 5
+  }
+}));
+
+exports.AppGridMenuSvgrepoCom = SvgAppGridMenuSvgrepoCom;
---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs'
+  },
+  external:[/@babel\/runtime/ ,'react'],
+  plugins: [resolve(), babel({ babelHelpers: 'runtime' , plugins: ["@babel/plugin-transform-runtime"] })]
+};
